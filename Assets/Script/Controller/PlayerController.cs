9b27f2cd6443fd32f2cdb7fb674679c47f92b281
cuongasd <tcuong554@gmail.com>
2023-11-03 00:33:03 +0700

    update

--- Assets/Script/Controller/PlayerController.cs
+++ Assets/Script/Controller/PlayerController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Joystick joystick;
+    public float moveSpeed = 5f;
+    [SerializeField] private CharacterController characterController;
+
+    public Animator animator;
+    public void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float horizontalMove = joystick.Horizontal;
+        float verticalMove = joystick.Vertical;
+        Vector3 moveDirection = new Vector3(horizontalMove, 0, verticalMove);
+        if (moveDirection != Vector3.zero)
+        {
+            animator.SetBool("walk", true);
+            animator.gameObject.transform.rotation = Quaternion.LookRotation(moveDirection);
+        }
+        else
+        {
+            animator.SetBool("walk", false);
+        }
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= moveSpeed;
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 }
